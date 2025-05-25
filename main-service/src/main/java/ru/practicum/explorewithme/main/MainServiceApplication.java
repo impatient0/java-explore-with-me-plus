da5c7f41ba082f8bb6111cc1ea6c54bbc3617605
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-05-26 01:09:44 +0300

    create the hit logging aspect

--- main-service/src/main/java/ru/practicum/explorewithme/main/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/explorewithme/main/MainServiceApplication.java
@@ -2,8 +2,10 @@ package ru.practicum.explorewithme.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum.explorewithme.main", "ru.practicum.explorewithme.stats.client"})
 public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
