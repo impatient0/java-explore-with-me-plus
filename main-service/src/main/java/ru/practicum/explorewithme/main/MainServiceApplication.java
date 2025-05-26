f2e4097afea797fbb23bceb5e1e9dee9bf0ba76b
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-05-26 03:06:37 +0300

    change the way StatsClient is pulled

--- main-service/src/main/java/ru/practicum/explorewithme/main/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/explorewithme/main/MainServiceApplication.java
@@ -2,10 +2,11 @@ package ru.practicum.explorewithme.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import ru.practicum.explorewithme.stats.client.config.StatsClientModuleConfiguration;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.practicum.explorewithme.main", "ru.practicum.explorewithme.stats.client"})
+@Import(StatsClientModuleConfiguration.class)
 public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
