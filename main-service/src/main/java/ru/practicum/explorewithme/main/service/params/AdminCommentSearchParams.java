80eb46d341f4678de3708dc1e0e4140afbcdb408
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-05-31 14:39:33 +0300

    add admin search params and update service interface

---
+++ main-service/src/main/java/ru/practicum/explorewithme/main/service/params/AdminCommentSearchParams.java
@@ -0,0 +1,14 @@
+package ru.practicum.explorewithme.main.service.params;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class AdminCommentSearchParams {
+    private final Long userId;
+    private final Long eventId;
+    private final Boolean isDeleted;
+}
